be223f77b673178c58867e2b861dfcbcd4066f33
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-02-15 17:34:14 +0100

    main.py EC2.py identation

--- EC2.py
+++ EC2.py
@@ -1,67 +1,63 @@
 import boto3
-
 from botocore.exceptions import ClientError
-
 import pandas as pd
-
 
 class EC2():
 
-	def __init__(self,region):
-		if region:
-        	    boto3.setup_default_session(region_name=region)
-		
-		self.ec2 = boto3.client('ec2')
-		self.FIELDS = ['Description', 'ID', 'Status', 'Tag_name', 'Tag_project', 'Creation_date', 'Region']
-		# TODO This is temporary, it should come in a base class
+    def __init__(self,region):
+        if region:
+            boto3.setup_default_session(region_name=region)
 
-		# TODO check if the connection was established
+        self.ec2 = boto3.client('ec2')
+        self.FIELDS = ['Description', 'ID', 'Status', 'Tag_name', 'Tag_project', 'Creation_date', 'Region']
+        # TODO This is temporary, it should come in a base class
+
+        # TODO check if the connection was established
 
 
-	def collect(self, services=[]):
-		"""
-		Get EC2 stuff
+    def collect(self, services=[]):
+        """
+        Get EC2 stuff
 
-		Parameters
-        ----------        
-        services: list
+            Parameters
+            ----------
+            services: list
             It contains the interesting services in EC2 such as machines, spot instances, ebs and so on.
             If empty, means all services
 
-		Returns: pandas' Dataframe
-		"""
-		try:
-			response = self.ec2.describe_instances()
+            Returns: pandas' Dataframe
+        """
+        try:
+            response = self.ec2.describe_instances()
+        except ClientError as e:
+            print("Exception in class " + self.__class__.__name__)
+            print(e.message)
 
-		except ClientError as e:
-			print("Exception in class " + self.__class__.__name__)
-			print(e.message)
+            return None
 
-			return None
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            return None
 
-		if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-			return None
+        # Pruning and formatting
+        formatted_list = []
 
-		# Pruning and formatting
-		formatted_list = []
-		
-		print("Processing %d elements." % len(response['Reservations']) )
+        print("Processing %d elements." % len(response['Reservations']) )
 
-		for item in response['Reservations']:
+        for item in response['Reservations']:
 
-			item_info = dict.fromkeys(self.FIELDS)
+            item_info = dict.fromkeys(self.FIELDS)
+            item_info['Description'] = item['Instances'][0]['InstanceType']
+            item_info['Creation_date'] = str(item['Instances'][0]['LaunchTime']).split(" ")[0]
+            item_info['ID'] = item['Instances'][0]['InstanceId']
+            item_info['Status'] = item['Instances'][0]['State']['Name']
+            item_info['Tag_name'] = item['Instances'][0]['Tags'][0].get('Name', None)
+            item_info['Tag_project'] = item['Instances'][0]['Tags'][0].get('Project', None)
+            item_info['Region'] = item['Instances'][0]['Placement']['AvailabilityZone']
 
-			item_info['Description'] = item['Instances'][0]['InstanceType']
-			item_info['Creation_date'] = str(item['Instances'][0]['LaunchTime']).split(" ")[0]
-			item_info['ID'] = item['Instances'][0]['InstanceId']
-			item_info['Status'] = item['Instances'][0]['State']['Name']
-			item_info['Tag_name'] = item['Instances'][0]['Tags'][0].get('Name', None)
-			item_info['Tag_project'] = item['Instances'][0]['Tags'][0].get('Project', None)
-			item_info['Region'] = item['Instances'][0]['Placement']['AvailabilityZone']
-
-			formatted_list.append(item_info)
+            formatted_list.append(item_info)
 
 
-		pruned = pd.DataFrame(formatted_list)
+        pruned = pd.DataFrame(formatted_list)
 
-		return pruned
+        return pruned
+
--- main.py
+++ main.py
@@ -5,26 +5,26 @@ from collect import collect_from_services
 
 if __name__ == '__main__':
 
-	# Services identified as potentially costly
-	services_required = ['ec2', 'ec2_ebs']
+    # Services identified as potentially costly
+    services_required = ['ec2', 'ec2_ebs']
 
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-o", "--output", required=False, help="Path to the written report (CSV file)")
-	ap.add_argument("--region", required=False, help="The region to use. Overrides config/env settings.")
-	#ap.add_argument("--all", required=False, help="Return info about all services listed in variable services (currently only EC2)")
-	#ap.add_argument("-v", "--verbose", required=False, help="Show info on terminal")
-	args = vars(ap.parse_args())
-	region = args["region"]
-	collected = collect_from_services(services_required,region)
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-o", "--output", required=False, help="Path to the written report (CSV file)")
+    ap.add_argument("--region", required=False, help="The region to use. Overrides config/env settings.")
+    #ap.add_argument("--all", required=False, help="Return info about all services listed in variable services (currently only EC2)")
+    #ap.add_argument("-v", "--verbose", required=False, help="Show info on terminal")
+    args = vars(ap.parse_args())
+    region = args["region"]
+    collected = collect_from_services(services_required,region)
 
-	if collected is not None:
+    if collected is not None:
+        if args["output"]:
+            with open(args["output"], 'w') as fout:
+                collected.to_csv(fout)
 
-		if args["output"]:
-			with open(args["output"], 'w') as fout:
-				collected.to_csv(fout)
+        #if "verbose" in args:
+        print(collected.head())
 
-		#if "verbose" in args:
-		print(collected.head())
+    else:
+        print("No info was collected.")
 
-	else:
-		print("No info was collected.")
